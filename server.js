b3fb82705f0e6c776c283185cf4e484ba6f8e1ae
Messuoh00 <houssemorsam1@gmail.com>
2022-07-02 18:38:35 +0100

    ok

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const expresslayouts=require('express-ejs-layouts')
 
 const indexroute=require('./routes/index')
 
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL,{usenewURLparser: true})
+
+const db = mongoose.connection
+
+db.on('error',error=>console.error('error'))
+db.on('open',()=>console.log('rahi tmchi yay connected to mongogdb:)'))
+
+
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
